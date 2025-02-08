3e50547101dcb1011b5a89de269be90591b9f09d
Kristina Faye <hellokristinafaye@gmail.com>
2025-02-08 11:56:55 -0800

    working thru codewars katas

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ console.log("Expelliarumus");
 // console.log(answer);
 
 
-[3, 2,  1]
+// [3, 2,  1]
 // function gimme(triplet) {
 
 //     if (triplet[0] < triplet[1] && triplet[0] > triplet[2]) {
@@ -27,21 +27,38 @@ console.log("Expelliarumus");
 //     }
 // }
 
-function gimme(a) {
-    return a.indexOf(a.concat().sort(function (a, b) { return a - b })[1])
-}
+// function gimme(a) {
+//     return a.indexOf(a.concat().sort(function (a, b) { return a - b })[1])
+// }
 
 // OK I think I get it??? 
 // the prompt is asking for the index of something (the middle number), so that's why you put ".indexOf".  So inside those parentheses, you sort the array as normal, and then the [1] is calling for the MIDDLE number after the sort. THEN the a.indexOf() returns the index of that MIDDLE number.  Very cool! 
 
-gimme([2, 3, 1])
+// gimme([2, 3, 1])
 
-console.log(gimme([2, 3, 1]))
+// console.log(gimme([2, 3, 1]))
 
-function repeatStr(n, s) {
-    return s.repeat(n);
-}
+// function repeatStr(n, s) {
+//     return s.repeat(n);
+// }
 
-console.log(repeatStr(3, "hello"))
+// console.log(repeatStr(3, "hello"))
 
 // TIL about the .repeat() method! 
+
+
+// function nbYear(p0, percent, aug, p) {
+
+//     for (var years = 0; p0 < p; years++) {
+//         p0 = Math.floor(p0 + p0 * percent / 100 + aug);
+//     }
+//     return years
+// }
+
+// console.log(nbYear(1500, 5, 100, 5000));
+
+function twoSort(s) {
+    return s.sort()[0].split('').join('***');
+}
+
+console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]));
